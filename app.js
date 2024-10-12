1eb784bfeb36b081b8edc92cfd66a220d6e4cf9e
G-Hae <sifialee@gmail.com>
2024-10-12 17:48:31 +0900

    feat: 기본 crud

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ connection.connect((err) => {
 });
 
 app.use(cors());
+app.use(express.json());
 
 app.listen(port, () => {
     console.log(`Backend server is running on http://localhost:${port}`);
@@ -35,5 +36,51 @@ app.get('/api/data', (req, res) => {
             return;
         }
         res.json(result);
+    });
+});
+
+app.post('/api/list', (req, res) => {
+    const { text } = req.body;
+    if (!text) {
+        return res.status(400).json({ error: '명언을 입력하세요.' });
+    }
+    const query = 'INSERT INTO oneline_list (text, reg_Id) VALUES (?,?)';
+    connection.query(query, [text, 1], (err, result) => {
+        if (err) {
+            return res.status(500).json({ error: '쿼리 실행 실패' });
+        }
+        res.status(201).json({ message: '데이터가 성공적으로 추가되었습니다.' });
+    });
+});
+
+app.delete('/api/list/:id', (req, res) => {
+    const { id } = req.params;
+    const query = 'DELETE FROM oneline_list WHERE id = ?';
+    connection.query(query, [id], (err, result) => {
+        console.log('result', result);
+        if (err) {
+            return res.status(500).json({ error: '쿼리 실행 실패' });
+        }
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ error: '명언이 없습니다' });
+        }
+        res.status(200).json({ message: '명언이 성공적으로 삭제되었습니다.' });
+    });
+});
+
+app.put('/api/list/:id', (req, res) => {
+    const { id } = req.params;
+    const { text } = req.body; // 클라이언트에서 보낸 텍스트를 가져옵니다.
+
+    const query = 'UPDATE oneline_list SET text = ?, reg_Id = ? WHERE id = ?';
+
+    connection.query(query, [text, 1, id], (err, result) => {
+        if (err) {
+            return res.status(500).json({ error: '쿼리 실행 실패' });
+        }
+        if (result.affectedRows === 0) {
+            return res.status(404).json({ error: '명언이 없습니다' });
+        }
+        res.status(200).json({ message: '명언이 성공적으로 수정되었습니다.' });
     });
 });
